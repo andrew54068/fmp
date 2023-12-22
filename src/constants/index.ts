66e6168bce3b21287e628bc0b95c99e1fd6870a7
moojing <starts789@gmail.com>
2023-12-22 19:16:16 +0800

    feat: switch to mainnet.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,16 +4,17 @@
 
 
 //testnet
-export const NonFungibleToken = "0x631e88ae7f1d7c20"
-export const Inscription = "0x564b229bd8380848"
-export const InscriptionMetadata = "0x564b229bd8380848"
+// export const NonFungibleToken = "0x631e88ae7f1d7c20"
+// export const Inscription = "0x564b229bd8380848"
+// export const InscriptionMetadata = "0x564b229bd8380848"
 
 // mainnet
-// export const NonFungibleToken = "0x1d7e57aa55817448"
-// export const Inscription = "0xa217c76f53a1a081"
-// export const InscriptionMetadata = "0xa217c76f53a1a081"
+export const NonFungibleToken = "0x1d7e57aa55817448"
+export const Inscription = "0xa217c76f53a1a081"
+export const InscriptionMetadata = "0xa217c76f53a1a081"
 
 // testnet 
-export const FLOW_SCAN_URL = 'https://testnet.flowdiver.io/tx/'
+// export const FLOW_SCAN_URL = 'https://testnet.flowdiver.io/tx/'
+
 // mainnet
-// export const FLOW_SCAN_URL = 'https://flowdiver.io/tx/'
+export const FLOW_SCAN_URL = 'https://flowdiver.io/tx/'
--- src/services/fcl/index.ts
+++ src/services/fcl/index.ts
@@ -2,16 +2,16 @@ import * as fcl from "@blocto/fcl";
 export const DEFAULT_APP_ID = "00000000-0000-0000-0000-000000000000";
 export const DAPP_ID = "776b8c25-1c11-43ba-a1ec-47b9281b8bc6"
 // mainnet
-// fcl.config({
-//   "accessNode.api": "https://rest-mainnet.onflow.org", // connect to Flow mainnet
-//   "discovery.wallet": `https://wallet-v2.blocto.app/${DEFAULT_APP_ID}/flow/authn` // use Blocto mainnet wallet
-// });
+fcl.config({
+  "accessNode.api": "https://rest-mainnet.onflow.org", // connect to Flow mainnet
+  "discovery.wallet": `https://wallet-v2.blocto.app/${DAPP_ID}/flow/authn` // use Blocto mainnet wallet
+});
 
 // testnet
-fcl.config({
-  'flow.network': 'testnet',
-  "accessNode.api": "https://rest-testnet.onflow.org", // connect to Flow mainnet
-  "discovery.wallet": `https://wallet-v2-dev.blocto.app/${DAPP_ID}/flow/authn`, // use Blocto mainnet wallet
-})
+// fcl.config({
+//   'flow.network': 'testnet',
+//   "accessNode.api": "https://rest-testnet.onflow.org", // connect to Flow mainnet
+//   "discovery.wallet": `https://wallet-v2-dev.blocto.app/${DAPP_ID}/flow/authn`, // use Blocto mainnet wallet
+// })
 
 console.log('fcl config!!')
