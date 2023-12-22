72cb8b3f259d24dd191106b1d427a2838891282b
moojing <starts789@gmail.com>
2023-12-22 19:30:37 +0800

    feat: switch network by env.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,16 @@
+const isMainnet = import.meta.env.VITE_NETWORK === 'mainnet'
 
-// these address are temporarily on testnet
-//@todo:  replace with mainnet addresses
+const testnetAddress = {
+  NonFungibleToken: "0x631e88ae7f1d7c20",
+  Inscription: "0x564b229bd8380848",
+  InscriptionMetadata: "0x564b229bd8380848"
+}
 
+const mainnetAddress = {
+  NonFungibleToken: "0x1d7e57aa55817448",
+  Inscription: "0xa217c76f53a1a081",
+  InscriptionMetadata: "0xa217c76f53a1a081"
+}
 
 //testnet
 // export const NonFungibleToken = "0x631e88ae7f1d7c20"
@@ -9,12 +18,12 @@
 // export const InscriptionMetadata = "0x564b229bd8380848"
 
 // mainnet
-export const NonFungibleToken = "0x1d7e57aa55817448"
-export const Inscription = "0xa217c76f53a1a081"
-export const InscriptionMetadata = "0xa217c76f53a1a081"
+// const NonFungibleToken = "0x1d7e57aa55817448"
+// const Inscription = "0xa217c76f53a1a081"
+// const InscriptionMetadata = "0xa217c76f53a1a081"
 
 // testnet 
 // export const FLOW_SCAN_URL = 'https://testnet.flowdiver.io/tx/'
 
-// mainnet
-export const FLOW_SCAN_URL = 'https://flowdiver.io/tx/'
+export const FLOW_SCAN_URL = isMainnet ? 'https://flowdiver.io/tx/' : 'https://testnet.flowdiver.io/tx/'
+export const { NonFungibleToken, Inscription, InscriptionMetadata } = isMainnet ? mainnetAddress : testnetAddress
--- src/services/fcl/index.ts
+++ src/services/fcl/index.ts
@@ -1,17 +1,17 @@
 import * as fcl from "@blocto/fcl";
+const isMainnet = import.meta.env.VITE_NETWORK === 'mainnet'
+
 export const DEFAULT_APP_ID = "00000000-0000-0000-0000-000000000000";
-export const DAPP_ID = "776b8c25-1c11-43ba-a1ec-47b9281b8bc6"
+export const FREE_FLOW_DAPP_ID = "776b8c25-1c11-43ba-a1ec-47b9281b8bc6"
+export const DAPP_ID = isMainnet ? FREE_FLOW_DAPP_ID : DEFAULT_APP_ID
+
+const NODE_API = isMainnet ? "https://rest-mainnet.onflow.org" : "https://rest-testnet.onflow.org";
+const WALLET_URL = isMainnet ? `https://wallet-v2.blocto.app/${DAPP_ID}/flow/authn` : `https://wallet-v2-dev.blocto.app/${DAPP_ID}/flow/authn`;
+
 // mainnet
 fcl.config({
-  "accessNode.api": "https://rest-mainnet.onflow.org", // connect to Flow mainnet
-  "discovery.wallet": `https://wallet-v2.blocto.app/${DAPP_ID}/flow/authn` // use Blocto mainnet wallet
+  "accessNode.api": NODE_API,
+  "discovery.wallet": WALLET_URL
 });
-
-// testnet
-// fcl.config({
-//   'flow.network': 'testnet',
-//   "accessNode.api": "https://rest-testnet.onflow.org", // connect to Flow mainnet
-//   "discovery.wallet": `https://wallet-v2-dev.blocto.app/${DAPP_ID}/flow/authn`, // use Blocto mainnet wallet
-// })
 
 console.log('fcl config!!')
