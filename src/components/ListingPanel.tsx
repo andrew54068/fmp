fe15fbfd2db8870b0b407a1e0a44f012fbe229d6
moojing <starts789@gmail.com>
2023-12-28 18:58:55 +0800

    feat: refine buying flow.

--- src/components/ListingPanel.tsx
+++ src/components/ListingPanel.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import { Box, SimpleGrid } from '@chakra-ui/react';
+import { Flex, Box, SimpleGrid } from '@chakra-ui/react';
 import Button from 'src/components/Button'
+import { InfoOutlineIcon } from '@chakra-ui/icons'
 import InscriptionsCard from 'src/components/InscriptionCard';
 import { ListingMetadata } from 'src/types';
 
@@ -39,7 +40,7 @@ const inscriptions = [
 export default function ListingPanel() {
   const [isBuying, setIsBuying] = useState(false);
   const [selectedInscriptions, setSelectedInscriptions] = useState<string[] | null>([]);
-  console.log('selectedInscriptions :', selectedInscriptions);
+  const [priceSummary, setPriceSummary] = useState(0);
   const hasSelected = selectedInscriptions?.length ?? 0 > 0;
 
   const handleBuyClick = () => {
@@ -56,12 +57,74 @@ export default function ListingPanel() {
       setSelectedInscriptions((prev) => [
         ... (prev || []),
         inscription.uuid]);
+      // Add price to summary
+      if (inscription?.price) {
+        setPriceSummary((prev) => prev + parseFloat(inscription?.price || ''));
+      }
     } else {
       setSelectedInscriptions(
         (prev) => (prev || [])?.filter((uuid) => uuid !== inscription.uuid));
+      // Remove price from summary
+      if (inscription?.price) {
+        setPriceSummary((prev) => prev - parseFloat(inscription?.price || ''));
+      }
+    }
+  };
+
+  const handleCancel = () => {
+    setIsBuying(false);
+    setSelectedInscriptions([]);
+    setPriceSummary(0);
+  };
+
+  const handleSendTransaction = () => {
+    //@todo add transaction logic 
+  }
+
+  const CallToActionButton = () => {
+    if (!isBuying) {
+      return <Button
+        colorScheme="blue"
+        onClick={handleBuyClick}
+        width={["100%", "auto"]}
+        display="inline-block" minW="200px">
+        Buy
+      </Button>
+    }
+    if (hasSelected) {
+      return <Box>
+        <Button
+          colorScheme="blue"
+          onClick={handleSendTransaction}
+          width={["100%", "auto"]}
+          bg="#01ef8b"
+          _hover={{
+            bg: "#01ef8b",
+            transform: "scale(0.98)"
+          }}
+        >
+          Buy {selectedInscriptions?.length} Items
+        </Button>
+        <Button
+          ml="space.m"
+          colorScheme="blue"
+          onClick={handleCancel}
+          width={["100%", "auto"]}
+        >
+          Cancel
+        </Button></Box>
     }
 
-  };
+    return <Button
+      ml="space.m"
+      colorScheme="blue"
+      onClick={handleCancel}
+      width={["100%", "auto"]}
+    >
+      Cancel
+    </Button>
+
+  }
 
   return (
     <Box p="16px">
@@ -83,11 +146,36 @@ export default function ListingPanel() {
         ))}
       </SimpleGrid>
 
-      <Box pos="fixed" bottom="0" left="0" right="0" p="space.m" maxWidth="820px" margin="0 auto">
-        <Button colorScheme="blue" onClick={handleBuyClick} display="inline-block">
-          {isBuying ? (hasSelected ? "Send The Transaction" : "Cancel") : "Buy"}
-        </Button>
+      <Box pos="fixed"
+        bottom="0"
+        left="0"
+        right="0"
+        width="100%"
+        bg="gray.800"
+      >
+        <Flex
+          alignItems="center"
+          justifyContent="space-between"
+          p="space.m"
+          maxWidth="820px"
+          margin="0 auto"
+          flexDirection={["column", "row"]}>
+          <Box opacity={isBuying ? "1" : "0"} mb={["16px", "0"]}>
+            <Flex fontSize="size.body.2" mb="space.2xs" color="gray.400" alignItems="center">
+              <InfoOutlineIcon />
+              <Box ml="space.3xs">
+                You can buy up to 50 items at a time.
+              </Box>
+            </Flex>
+            {
+              <Box fontSize="size.body.1">
+                You are buying {selectedInscriptions?.length} items for {priceSummary} Flow
+              </Box>
+            }
+          </Box>
+          <CallToActionButton />
+        </Flex>
       </Box>
-    </Box>
+    </Box >
   );
 }
--- src/components/Marketplace.tsx
+++ src/components/Marketplace.tsx
@@ -3,7 +3,7 @@ import ListingPanel from 'src/components/ListingPanel';
 
 
 export default function Marketplace() {
-  return <Box bg="gray.700" mt="75px" minH="calc(100vh - 75px)" padding="16px" >
+  return <Box bg="gray.700" mt="75px" minH="calc(100vh - 75px)" padding="16px" pt="60px" >
     <Tabs colorScheme="whiteAlpha" variant="marketplace">
       <TabList>
         <Tab>
