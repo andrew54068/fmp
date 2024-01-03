c2bdbd6df1482c988b69192ce8cf30adf9711a84
andrew54068 <andrew0424718012@yahoo.com.tw>
2024-01-03 15:39:17 +0800

    feat: add total trading volume.

--- src/components/Marketplace.tsx
+++ src/components/Marketplace.tsx
@@ -1,28 +1,63 @@
-import { Box, Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
-import ListingPanel from 'src/components/ListingPanel';
-import PersonalPanel from './PersonalPanel';
-
+import {
+  Box,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Flex,
+} from "@chakra-ui/react";
+import ListingPanel from "src/components/ListingPanel";
+import PersonalPanel from "./PersonalPanel";
+import { useEffect, useState } from "react";
+import { sendScript } from "src/services/fcl/send-script";
+import { getBalanceScript } from "src/utils/getScripts";
+import BigNumber from "bignumber.js";
 
 export default function Marketplace() {
-  return <Box bg="gray.700" mt="75px" minH="calc(100vh - 75px)" padding="16px" pb="172px" pt="60px" >
-    <Tabs colorScheme="whiteAlpha" variant="marketplace">
-      <TabList>
-        <Tab>
-          Listing
-        </Tab>
-        <Tab>
-          My Items
-        </Tab>
-      </TabList>
+  const [flowBalance, setFlowBalance] = useState("");
+  
+  useEffect(() => {
+    const fetchRoyaltyFeeBalance = async () => {
+      const balance: string = await sendScript(getBalanceScript(), (arg, t) => [
+        arg("0x81bfc5cc7d1e0c74", t.Address),
+      ]);
+      const value = new BigNumber(balance).multipliedBy(BigNumber(40))
+      setFlowBalance(value.toString())
+    }
 
-      <TabPanels>
-        <TabPanel>
-          <ListingPanel />
-        </TabPanel>
-        <TabPanel>
-          <PersonalPanel />
-        </TabPanel>
-      </TabPanels>
-    </Tabs>
-  </Box >
+    const refreshTradingVolume = setInterval(function() {
+      fetchRoyaltyFeeBalance()
+    }, 5000);
+    return () => clearInterval(refreshTradingVolume)
+  }, []);
+
+
+  return (
+    <Box
+      bg="gray.700"
+      mt="75px"
+      minH="calc(100vh - 75px)"
+      padding="16px"
+      pb="172px"
+      pt="60px"
+    >
+      <Flex p='20px' borderRadius='md' bg='white' margin="20px 0px" fontSize="size.heading.3" color="gray.700">Total Trading Volume: {flowBalance} Flow</Flex>
+      <Tabs colorScheme="whiteAlpha" variant="marketplace">
+        <TabList>
+          <Tab>Listing</Tab>
+          <Tab>My Items</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <ListingPanel />
+          </TabPanel>
+          <TabPanel>
+            <PersonalPanel />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Box>
+  );
 }
