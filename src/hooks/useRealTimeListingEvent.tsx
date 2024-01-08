491dee8ac98fea069a116fd0c414f5c60ff6cd91
moojing <starts789@gmail.com>
2024-01-08 17:59:12 +0800

    temp

--- src/hooks/useRealTimeListingEvent.tsx
+++ src/hooks/useRealTimeListingEvent.tsx
@@ -1,7 +1,7 @@
 import { useState, useCallback, useEffect } from 'react';
 import useInterval from './useInterval';
 import * as fcl from "@blocto/fcl";
-import { LISTING_EVENT_NAME, LATEST_BLOCK_HEIGHT_KEY, FLOW_SCAN_URL } from 'src/constants'
+import { LISTING_EVENT_NAME, FLOW_SCAN_URL } from 'src/constants'
 import { useToast, Box, Flex } from '@chakra-ui/react'
 import { Link } from "react-router-dom";
 
@@ -12,11 +12,37 @@ export default function useRealTimeListingEvent({ footerPosition }: {
   }
 }) {
 
+  const [prevBlockHeight, setPrevBlockHeight] = useState<number>(0)
   const [realTimeListingEvent, setRealTimeListingEvent] = useState<any[]>([])
   const toast = useToast()
 
-  const getRealTimeListingEvent = useCallback(async () => {
-    const prevLatestBlockHeight = localStorage.getItem(LATEST_BLOCK_HEIGHT_KEY)
+  const getListingEventByRange = useCallback(async (fromBlockHeight: number, toBlockHeight: number) => {
+
+    const listingEvents = await fcl
+      .send([
+        fcl.getEventsAtBlockHeightRange(
+          LISTING_EVENT_NAME,
+          fromBlockHeight,
+          toBlockHeight
+        ),
+      ]).then(fcl.decode)
+
+    const blockEventMap: Record<string, any[]> = {}
+
+    console.log(`💥 listingEvents: ${JSON.stringify(listingEvents, null, '  ')}`);
+    if (Array.isArray(listingEvents) && listingEvents.length > 0) {
+      const filteredEvents = listingEvents
+        .filter(event => event.data.nftType.typeID === "A.88dd257fcf26d3cc.Inscription.NFT" && event.data.purchased)
+      filteredEvents.forEach(event => {
+        blockEventMap[event.transactionId] = blockEventMap[event.transactionId] ? [...blockEventMap[event.transactionId], event] : [event]
+      })
+      if (filteredEvents.length > 0) {
+        return Object.values(blockEventMap)
+      }
+    }
+  }, [])
+
+  const getLatestTenEvent = useCallback(async () => {
     const { height: latestBlockHeight } = await fcl
       .send([
         fcl.getBlock(true), // isSealed = true
@@ -25,38 +51,31 @@ export default function useRealTimeListingEvent({ footerPosition }: {
 
 
     if (!latestBlockHeight) return
-    if (`${prevLatestBlockHeight}` === `${latestBlockHeight}`) return
+    if (`${prevBlockHeight}` === `${latestBlockHeight}`) return
 
-    localStorage.setItem(LATEST_BLOCK_HEIGHT_KEY, latestBlockHeight);
-    const latestListingEvents = await fcl
-      .send([
-        fcl.getEventsAtBlockHeightRange(LISTING_EVENT_NAME, prevLatestBlockHeight || latestBlockHeight, latestBlockHeight),
-      ]).then(fcl.decode)
+    setPrevBlockHeight(latestBlockHeight)
 
-    const blockEventMap: Record<string, any[]> = {}
+    const fromBlock = latestBlockHeight
+    const toBlock = latestBlockHeight - 250
 
-    console.log(`💥 latestListingEvent: ${JSON.stringify(latestListingEvents, null, '  ')}`);
-    if (Array.isArray(latestListingEvents) && latestListingEvents.length > 0) {
-      const filteredEvents = latestListingEvents
-        .filter(event => event.data.nftType.typeID === "A.88dd257fcf26d3cc.Inscription.NFT" && event.data.purchased)
-      filteredEvents.forEach(event => {
-        blockEventMap[event.transactionId] = blockEventMap[event.transactionId] ? [...blockEventMap[event.transactionId], event] : [event]
-      })
-      if (filteredEvents.length > 0) {
-        setRealTimeListingEvent(Object.values(blockEventMap))
-      }
+    // while (realTimeListingEvent.length < 10) {
+    const latestListingEvent = await getListingEventByRange(fromBlock, toBlock)
+    if (Array.isArray(latestListingEvent) && latestListingEvent.length > 0) {
+      setRealTimeListingEvent(latestListingEvent)
     }
-
-  }, [])
-
-  useInterval(() => {
-    getRealTimeListingEvent()
-  }, 3000)
-
+    // }
+  }, [getListingEventByRange, prevBlockHeight])
 
   useEffect(() => {
-    getRealTimeListingEvent()
-  }, [getRealTimeListingEvent])
+    getLatestTenEvent()
+  }, [getLatestTenEvent])
+
+  // useInterval(() => {
+  // getLatestTenEvent()
+  // }, 3000)
+
+
+
 
   useEffect(() => {
     if (realTimeListingEvent.length > 0) {
@@ -67,13 +86,13 @@ export default function useRealTimeListingEvent({ footerPosition }: {
           duration: 10 * 1000,
           containerStyle: {
             position: 'relative',
-            bottom: `${footerPosition.bottom + 50 * index}px`,
+            bottom: `${footerPosition.bottom}px`,
             left: `${footerPosition.left}px`,
           },
           render: () => (
             <Flex>
               <Box color='gray.500' p={2} bg='gray.800' borderRadius="4px">
-                <Link 
+                <Link
                   to={FLOW_SCAN_URL + (events[0].transactionId as string)}
                   target="_blank"
                   style={{ textDecoration: "underline" }}
